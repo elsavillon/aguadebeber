724e3ba1400671191207daff632f8eb17b19a734
Elsa Villon <144967593+elsavillon@users.noreply.github.com>
2024-06-21 00:11:25 -0300

    Atualizações index

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,27 +6,27 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('jarroabacaxi')
+@app.route('/jarroabacaxi')
 def jarroabacaxi():
     return render_template('jarroabacaxi.html')
 
-@app.route('copostanley')
+@app.route('/copostanley')
 def copostanley():
     return render_template('copostanley.html')
 
-@app.route ('copoamericano')
+@app.route('/copoamericano')
 def copoamericano():
     return render_template('copoamericano.html')
 
-@app.route ('coporequeijao')
+@app.route('/coporequeijao')
 def coporequeijao():
     return render_template('coporequeijao.html')
 
-@app.route ('garrafamole')
+@app.route('/garrafamole')
 def garrafamole():
     return render_template('garrafamole.html')
 
-@app.route ('sobre')
+@app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
 
